6646ec8a14ca7dd4a3ba30c90dfde9f640cb38c1
YuTengjing <ytj2713151713@gmail.com>
2024-03-13 09:17:32 +0800

    chore: inject content scripts at any website

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -4,7 +4,8 @@ import { toggleExtension } from './toggleExtension';
 import { onCommand } from './utils/message';
 
 export default defineContentScript({
-    matches: ['https://github.com/*', 'https://www.npmjs.com/*'],
+    matches: ['<all_urls>'],
+    runAt: 'document_idle',
     cssInjectionMode: 'ui',
     async main(ctx) {
         onCommand('toggleExtension', async () => {
